45d25cd1093763554da00bb05570ca95f6ac38a3
Daniel <danilserkiz465@gmail.com>
2025-03-18 13:57:54 +0000

    added checksum function and ability to send ICMP packets using RAW socket

--- main.cpp
+++ main.cpp
@@ -1,8 +1,71 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <netinet/ip_icmp.h>
+#include <unistd.h>
+
+unsigned short checksum(void *, int);
 
 int main(int argc, char *argv[])
 {
-    int sockfd = socket(AF_INET,SOCK_RAW,0);  
+    int sockfd = socket(AF_INET,SOCK_RAW,IPPROTO_ICMP);  
+
+    char packet[1024];
+    struct sockaddr_in dest;
+    struct icmphdr *icmp_heder = (struct icmphdr *)packet;
+
+    memset(&dest,0,sizeof(dest));
+    dest.sin_family=AF_INET;
+    dest.sin_addr.s_addr=inet_addr(argv[1]);
+
+    memset(packet,0,sizeof(packet));
+    icmp_heder->type=ICMP_ECHO;
+    icmp_heder->code=0;
+    icmp_heder->un.echo.id=0;
+    icmp_heder->un.echo.sequence=1;
+    icmp_heder->checksum=0;
+    icmp_heder->checksum = checksum(icmp_heder, sizeof(packet));
+ 
+    socklen_t destlen=sizeof(dest); 
+    sendto(sockfd,packet,sizeof(packet),0,(struct sockaddr *)&dest,destlen);
+    printf("icmp echo sent to %s\n",argv[1]);
+ 
+    char buf[1024];
+    struct sockaddr_in from;
+    socklen_t fromlen=sizeof(from);
+ 
+    if (recvfrom(sockfd,buf,sizeof(buf),0,(struct sockaddr *)&from ,&fromlen)!=-1)
+    {
+        printf("recived reply from %s\n",inet_ntoa(from.sin_addr));
+    }
+    else
+    {
+        printf("Error reciving ICMP reply\n");
+    }
+
+    close(sockfd);
     return 0;
+}
+
+
+unsigned short checksum(void *packet_header, int packet_len) {
+    unsigned short *buf = (unsigned short*)packet_header;
+    unsigned int sum = 0;
+
+    for (int i=0; i < packet_len/2; i++) 
+    {
+        sum += buf[i];
+    }
+ 
+    if (packet_len % 2 == 1) 
+    {
+         sum += ((unsigned char*)buf)[packet_len-1];
+    }
+
+    sum = (sum >> 16) + (sum & 0xFFFF);
+    sum += (sum >> 16);
+ 
+    return (unsigned short)~sum;
 }
